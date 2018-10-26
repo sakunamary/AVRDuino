54e732ccdc1268380392bee5294bb2acec08cc96
Tim <sakunamary@gmail.com>
2018-10-26 11:28:27 +0800

    add new codeing and notes

--- avrduino.sh
+++ avrduino.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 config="/etc/avrdude.conf";
+# $1 means board type 
+# $2 means programmer type usbtiny, arduino as ISP e.g.
+# $3 means usb port or serial port. for avrdude 6.0 or above ,you can us "lsusb" and than get usb info as following format "usb:bus:device".avrdude will use "-P usb:001:001"to connect usb device like USBTinyISP  
+
 
 case "$1" in
         uno)
                 #UNO
-                avrdude -C$config -v -patmega328p -carduino -P$2 -b115200 -D -Uflash:w:$3:i
+                avrdude -C $config -v -p atmega328p -c $2  -P $3 -b115200 -D -U flash:w:$4:i
                 ;;
 
         nano)
                 #NANO
-                avrdude -C$config -v -patmega328p -carduino -P$2 -b57600 -D -Uflash:w:$3:i
+                avrdude -C $config -v -p atmega328p -c $2 -P $3 -b57600 -D -U flash:w:$4:i
                 ;;
 
         pro-mini)
                 #PRO-MINI
-                avrdude -C$config -v -patmega328p -carduino -P$2 -b57600 -D -Uflash:w:$3:i
+                avrdude -C $config -v -p atmega328p -c $2 -P$3 -b57600 -D -U flash:w:$4:i
                 ;;
 
         mega)
                 #MEGA 2560
-                avrdude -C$config -v -patmega2560 -cwiring -P$2 -b115200 -D -Uflash:w:$3:i
+                avrdude -C $config -v -p atmega2560 -c $2 -P $3 -b115200 -D -Uflash:w:$4:i
                 ;;
         install)
                 #
